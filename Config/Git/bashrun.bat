e8fdef43dc7051c2a24f549a2cc21da5f9082e8d
Adrian Istrate <adrian.istrate@yahoo.com>
2010-08-17 22:05:04 +0200

    (corr).

--- Config/Git/bashrun.bat
+++ Config/Git/bashrun.bat
@@ -2,7 +2,7 @@
 
 :: Use this batch file to associate a file extension (e.g., .sh) with bash,
 :: by setting it as target for "Open With" in Windows Explorer.
-:: It will login first, so that ssh-agent will work properly.
+:: It will login the user first, so that ssh-agent will work properly.
 
 sh --login -i -c 'echo; echo; echo; %1'
 
