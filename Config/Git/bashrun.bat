f8d0a5eb3c2fa7da5dbf556fccb07691391a9a96
aistrate <adr_istrate@yahoo.com>
2010-09-17 21:57:56 +0200

    bashrun script: change drive and path before launching the shell.

--- Config/Git/bashrun.bat
+++ Config/Git/bashrun.bat
@@ -4,6 +4,10 @@
 :: by setting it as target for "Open With" in Windows Explorer.
 :: It will login the user first, so that ssh-agent will work properly.
 
+:: Change drive and path
+for %%i in (%1) do %%~di
+for %%i in (%1) do cd %%~pi
+
 sh --login -i -c 'echo; echo; echo; %1'
 
 echo.
